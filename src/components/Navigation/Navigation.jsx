b2fe6b74df9d45b30a37c050e745e928efb6909c
Alyona <polova_alyona0907@ukr.net>
2023-10-29 21:44:06 +0100

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,10 @@
-import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from 'redux/auth/selectors';
 import { PermanentMenu, StyledLink } from './Navigation.styled';
 
 export const Navigation = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
   return (
     <PermanentMenu>
       <StyledLink to="/">Home</StyledLink>
-      {isLoggedIn && <StyledLink to="/contacts">Contacts</StyledLink>}
+      <StyledLink to="/contacts">Contacts</StyledLink>
     </PermanentMenu>
   );
 };
